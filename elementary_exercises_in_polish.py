175cdc4c6443ba92ce1b9c40aec5e4c24241d029
Mateusz Konieczny <matkoniecz@gmail.com>
2020-11-19 10:55:01 +0100

    switch to actually used code

--- elementary_exercises_in_polish.py
+++ elementary_exercises_in_polish.py
@@ -4,12 +4,18 @@ from examgen import worksheet, make_quotient_rule_prob
 # make an exam with a filename and title
 myexam = worksheet("zadania", "Zadania", savetex=True)
 
-# add some problem sections 
-myexam.add_section("Simple linear equations", 50, "",
+
+myexam.add_section("Simple linear equations", 40, "",
                    "Równania do rozwiązania, wykonaj sprawdzenie poprawności rozwiązania. Wynik niekoniecznie jest liczbą całkowitą.")
 
-myexam.add_section("Linear equations", 5, "",
+"""
+# add some problem sections 
+myexam.add_section("Simple linear equations", 70, "",
                    "Równania do rozwiązania, wykonaj sprawdzenie poprawności rozwiązania. Wynik niekoniecznie jest liczbą całkowitą.")
+
+myexam.add_section("Linear equations", 6, "",
+                   "Równania do rozwiązania, wykonaj sprawdzenie poprawności rozwiązania. Wynik niekoniecznie jest liczbą całkowitą.")
+"""
 
 # generate the exam and solutions pdf
 myexam.write()
